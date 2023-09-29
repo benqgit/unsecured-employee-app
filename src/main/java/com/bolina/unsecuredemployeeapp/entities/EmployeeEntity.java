66542f77ef2a61822057bdace47d05f586f1ee23
benqolina <benqolina@gmail.com>
2023-09-29 20:22:43 +0100

    EmployeeController added

--- src/main/java/com/bolina/unsecuredemployeeapp/entities/EmployeeEntity.java
+++ src/main/java/com/bolina/unsecuredemployeeapp/entities/EmployeeEntity.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "employees")
-public class Employee {
+public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
