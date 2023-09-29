effd8fed2808f94fcffc584bdc8a468a43f2700a
benqolina <benqolina@gmail.com>
2023-09-29 19:29:29 +0100

    first commit

---
+++ src/main/java/com/bolina/unsecuredemployeeapp/UnsecuredEmployeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.bolina.unsecuredemployeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecuredEmployeeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnsecuredEmployeeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bolina/unsecuredemployeeapp/UnsecuredEmployeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolina.unsecuredemployeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsecuredEmployeeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
