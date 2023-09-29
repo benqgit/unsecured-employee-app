2c6a97df6684a637d9017124049444a2632d239b
benqolina <benqolina@gmail.com>
2023-09-29 19:50:14 +0100

    Employee Entity created & spring-jpa dependency added

---
+++ src/main/java/com/bolina/unsecuredemployeeapp/entities/Employee.java
@@ -0,0 +1,19 @@
+package com.bolina.unsecuredemployeeapp.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private boolean isActive;
+    private int level;
+
+}
